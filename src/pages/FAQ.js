f5df0720d94aeb9407d43769f08e61d17d14ecce
GoodluckMlungusye <goodluckmlungusye@gmail.com>
2024-07-26 13:24:13 +0300

    FAQ mapping

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import faqs from '../data/faq'
 
 const FAQ = () => {
+
+  const faqBlock = faqs.map((faq,index) => (
+    <div class="faq-container" key={index}>
+        <div class="faq-item">
+            <span class="faq-qn">{faq.question}</span>
+            <p class="faq-ans">{faq.answer}</p>
+        </div>
+    </div>
+  ));
+    
+
   return (
     <section class="section">
 
@@ -10,98 +22,7 @@ const FAQ = () => {
     </div>
 
     <div class="faq-items-container">
-
-        <div class="faq-container">
-            <div class="faq-item">
-                <span class="faq-qn">Interested in certification for the courses you have completed?</span>
-                <p class="faq-ans">Certification is granted upon completion of the learning activities for each course which certifies the acquisition of specific competencies, through performance assessments. The UN Habitat learn platform grants certification to those who have completed the course with a final scenario-based, competency-based performance evaluation of 80 percent or higher.</p>
-            </div>
-        </div>
-        
-        <div class="faq-container">
-            <div class="faq-item">
-                <span class="faq-qn">How can I register?</span>
-                <p class="faq-ans">Registration is free! Simply click on the "Register" button in the upper right corner of the landing page and fill in the form. Be sure to provide a valid email address. After registration, you will receive a confirmation email to validate your address and complete the registration process.</p>
-            </div>
-        </div>
-        
-        <div class="faq-container">
-            <div class="faq-item">
-                <span class="faq-qn">I forgot my password. How can I reset it?</span>
-                <p class="faq-ans">Click on the “Forgot username or password?” link on the Login page and then enter the email address you used when you registered. A new email will be sent to you with instructions to reset your password.</p>
-            </div>
-        </div>
-        
-        <div class="faq-container">
-            <div class="faq-item">
-                <span class="faq-qn">I cannot login to the website. What should I do?</span>
-                <p class="faq-ans">If you cannot login, try resetting your password by clicking on the “Forgot username or password?” link on the Login page and enter the email you used when you registered. A new email will be sent to this address with instructions to reset your password. If you follow these instructions and you still cannot log back in, you can contact us via email: <a href="mailto:unhabitat-learn@un.org">unhabitat-learn@un.org</a>. Make sure you provide us with the email address you used when you registered with us.</p>
-            </div>
-        </div>
-        
-        <div class="faq-container">
-            <div class="faq-item">
-                <span class="faq-qn">I forgot my password. Can you send it to me?</span>
-                <p class="faq-ans">For security reasons, we don’t have access to your password and are unable to send it to you. However, you can reset your password by clicking on the “Forgot username or password?” link on the Login page, and then enter the email address you used when you registered. A new email will be sent to this address with instructions to reset your password.</p>
-            </div>
-        </div>
-        
-        <div class="faq-container">
-            <div class="faq-item">
-                <span class="faq-qn">Can I update my personal information?</span>
-                <p class="faq-ans">You can easily update most of your personal information, including your email and password. Your personal information page is accessed by clicking on your username in the upper right corner (near the Log-out link). This should be visible when you are logged in.</p>
-            </div>
-        </div>
-        
-        <div class="faq-container">
-            <div class="faq-item">
-                <span class="faq-qn">When can I start my course and is there any cost associated with it?</span>
-                <p class="faq-ans">You can start a course anytime and anywhere. All courses available on the learn platform are completely free and self-paced. We offer all the multilingual eLearning courses free of charge, as a global public good.</p>
-            </div>
-        </div>
-        
-        <div class="faq-container">
-            <div class="faq-item">
-                <span class="faq-qn">I have logged in. How do I take a course?</span>
-                <p class="faq-ans">Once you are logged in, click on “Courses” in the menu and then select the course of your interest. All the courses are free of charge.</p>
-            </div>
-        </div>
-        
-        <div class="faq-container">
-            <div class="faq-item">
-                <span class="faq-qn">When I click on the link to take the course, nothing happens. What do I do?</span>
-                <p class="faq-ans">Most of our courses and activities are in SCORM format and may open in a separate popup window. In some cases, you or your browser could have enabled a popup blocking feature. It is normally not difficult to disable this feature. Please refer to your browser’s website for directions on how to disable popup blocking.</p>
-            </div>
-        </div>
-        
-        <div class="faq-container">
-            <div class="faq-item">
-                <span class="faq-qn">What software do I need to run the course?</span>
-                <p class="faq-ans">You will need the Google Chrome browser to run the course on your computer. You will need Moodle mobile app to run the courses on your mobile phone. Download Moodle mobile app from app store or play store.</p>
-            </div>
-        </div>
-        
-        <div class="faq-container">
-            <div class="faq-item">
-                <span class="faq-qn">Can I take a course on my tablet or mobile device?</span>
-                <p class="faq-ans">Yes, you can take a course on your phone. You will need Moodle mobile app to run the courses on your mobile phone. Download Moodle mobile app from app store or play store.</p>
-            </div>
-        </div>
-        
-        <div class="faq-container">
-            <div class="faq-item">
-                <span class="faq-qn">I have a bad internet connection. Can I download the course and take it offline?</span>
-                <p class="faq-ans">Yes, you can. You can download the course through Moodle mobile app and take it offline. You will need Moodle mobile app to run the courses on your mobile phone. Download Moodle mobile app from app store or play store.</p>
-            </div>
-        </div>
-        
-        <div class="faq-container">
-            <div class="faq-item">
-                <span class="faq-qn">I have a bad internet connection. Is it possible to send me the course on a CD-ROM or USB key?</span>
-                <p class="faq-ans">Unfortunately, we do not offer this service.</p>
-            </div>
-        </div>
-
+        {faqBlock}
     </div>
     
 </section>
