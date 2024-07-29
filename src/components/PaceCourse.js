e763383291115a2c50887c7dd446d7b36dc80a04
GoodluckMlungusye <goodluckmlungusye@gmail.com>
2024-07-29 10:27:41 +0300

    active style for selected tab

--- src/components/PaceCourse.js
+++ src/components/PaceCourse.js
@@ -1,58 +1,21 @@
-// import React, { useState, useEffect, useRef } from 'react';
 import React, { useState} from 'react';
-// import Isotope from 'isotope-layout';
 import animatedInfo from '../data/animated_info'
 
 function PaceCourse() {
-
-    // const galleryRef = useRef(null);
-    // const $galleryContainer = useRef(null);
     const [activeValue, setActiveValue] = useState(animatedInfo[0].value)
     const [currentLabel, setCurrentLabel] = useState(animatedInfo[0].label);
     const [currentDescription, setCurrentDescription] = useState(animatedInfo[0].description);
     const [currentIcon, setCurrentIcon] = useState(animatedInfo[0].icon);
-    // const [renderObject, setRenderObject] = useState()
-
-
-    // useEffect(() => {
-    //     // Initialize Isotope when component mounts
-    //     $galleryContainer.current = new Isotope(galleryRef.current, {
-    //     itemSelector: '.item',
-    //     layoutMode: 'fitRows',
-    //     });
-
-    //     return () => {
-    //     // Clean up Isotope instance when component unmounts
-    //     $galleryContainer.current.destroy();
-    //     };
-    // }, []);
 
     const handleFilter = (object) => {
-        // $galleryContainer.current.arrange({ filter: value });
         setActiveValue(object.value);
         setCurrentLabel(object.label);
         setCurrentDescription(object.description);
         setCurrentIcon(object.icon);
     };
 
-    // useEffect(() => {
-    //     if (object) {
-    //       handleFilter(object);
-    //     }
-    //   }, [object]); // Depend on `object`
-      
-
-    // const paceSelector = animatedInfo.map((animeInfo) => (
-    //     <div className={`selector-head ${activeValue === animeInfo.value? "active": " "}`} key={animeInfo.value} onClick={() => handleFilter(`.${animeInfo.value}`, animeInfo)}>
-    //         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-    //             <path strokeLinecap="round" strokeLinejoin="round" d={animeInfo.icon} />
-    //         </svg>
-    //         <span>{animeInfo.label}</span>
-    //     </div>
-    // ));
-
     const paceSelector = animatedInfo.map((animeInfo) => (
-        <div className={`selector-head ${activeValue === animeInfo.value? "selector-head-active": " "}`} key={animeInfo.value} onClick={() => handleFilter(animeInfo)}>
+        <div className={`selector-head ${activeValue === animeInfo.value? "active": " "}`} key={animeInfo.value} onClick={() => handleFilter(animeInfo)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d={animeInfo.icon} />
             </svg>
--- src/data/animated_info.js
+++ src/data/animated_info.js
@@ -1,34 +1,34 @@
 const animatedInfo = [
     {
-        value: "pace",
-        label: "Self-paced courses",
-        description: "Learn platform offers multilingual self-paced e-learning courses, accessible free of charge online and on the moodle mobile app. The courses allow you to learn about a subject based on your individual needs and interests, and the format allows you to study at your pace, from any location and at any time.",
-        icon: "M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+    value: "pace",
+    label: "Self-paced courses",
+    description: "Learn platform offers multilingual self-paced e-learning courses, accessible free of charge online and on the moodle mobile app. The courses allow you to learn about a subject based on your individual needs and interests, and the format allows you to study at your pace, from any location and at any time.",
+    icon: "M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
     },
 
     {
-    value: "pace",
+    value: "webinars",
     label: "International Technical Webinars",
     description: "Lorem1, ipsum dolor sit amet consectetur adipisicing elit. Doloribus accusamus rerum vero laboriosam voluptate sequi, at soluta quis numquam dolore! Doloribus accusamus rerum vero laboriosam",
     icon: "M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
     },
 
     {
-    value: "pace",
+    value: "tutorial",
     label: "Online Tutorial Courses",
     description: "Lorem2, ipsum dolor sit amet consectetur adipisicing elit. Doloribus accusamus rerum vero laboriosam voluptate sequi, at soluta quis numquam dolore! Doloribus accusamus rerum vero laboriosam",
     icon: "M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125Z"
     },
 
     {
-    value: "pace",
+    value: "mobile",
     label: "Mobile Learning",
     description: "Lorem3, ipsum dolor sit amet consectetur adipisicing elit. Doloribus accusamus rerum vero laboriosam voluptate sequi, at soluta quis numquam dolore! Doloribus accusamus rerum vero laboriosam",
     icon: "M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
     },
     
     {
-    value: "pace",
+    value: "university",
     label: "University Learning Programmers",
     description: "Lorem4, ipsum dolor sit amet consectetur adipisicing elit. Doloribus accusamus rerum vero laboriosam voluptate sequi, at soluta quis numquam dolore! Doloribus accusamus rerum vero laboriosam",
     icon: "M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
